10749dbdd9b224888704886f011c92d53e4a5451
JeongGod <jeonggu223@gmail.com>
2020-07-16 02:16:01 +0900

    Math : divisor

---
+++ Other_Problem/1037.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    int a[51];
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) scanf("%d", &a[i]);
+    sort(a,a+n);
+    printf("%d\n", a[0]*a[n-1]);
+    return 0;
+}
